1ff2fd5c61754e61c45116372523486be95a3a71
manuel cau <manuel.cau106@gmail.com>
2025-07-31 18:26:27 +0200

    feat: improve notification UI with initial get and subscribe listener

--- example/example.js
+++ example/example.js
@@ -1,55 +1,55 @@
 import { createNotificationManager } from "../dist/notificationManager.js";
 
 const manager = createNotificationManager();
+const notificationBox = document.querySelector(".notifications-box");
 
-async function renderNotifications() {
-  const list = await manager.get();
-  const notificationBox = document.querySelector(".notifications-box");
-
-  notificationBox.style.display = list.length > 0 ? "flex" : "none";
+function newNotification(notification) {
+  notificationBox.style.display = "flex";
 
   let ul = notificationBox.querySelector("ul");
   if (!ul) {
     ul = document.createElement("ul");
     notificationBox.appendChild(ul);
-  } else {
-    ul.innerHTML = "";
   }
 
-  list.forEach((notification) => {
-    const li = document.createElement("li");
-    li.textContent = `${notification.data.title} - ${notification.data.message} `;
-    li.style.color = "white";
-    li.style.fontWeight = "bold";
-    li.style.textDecoration = "none";
+  const li = document.createElement("li");
+  li.textContent = `${notification.data.title} - ${notification.data.message}`;
+  li.style.color = "white";
+  li.style.fontWeight = "bold";
 
-    if (notification.readAt) {
-      const readTime = document.createElement("small");
-      const time = new Date(notification.readAt).toLocaleTimeString();
-      readTime.textContent = ` (Read at ${time})`;
-      li.appendChild(readTime);
-      li.style.fontWeight = "200";
-      readTime.style.marginLeft = "10px";
-      readTime.style.fontSize = "10px";
-      readTime.style.color = "white";
-    } else {
-      const readButton = document.createElement("button");
-      readButton.textContent = "Mark as read";
-      readButton.style.marginLeft = "10px";
-      readButton.style.padding = "10px";
-      readButton.addEventListener("click", async () => {
-        await manager.setRead(notification.id);
-        renderNotifications();
-      });
-      li.appendChild(readButton);
-    }
+  if (notification.readAt) {
+    const readTime = document.createElement("small");
+    const time = new Date(notification.readAt).toLocaleTimeString();
+    readTime.textContent = ` (Read at ${time})`;
+    readTime.style.marginLeft = "10px";
+    readTime.style.fontSize = "10px";
+    readTime.style.color = "white";
+    li.appendChild(readTime);
+    li.style.fontWeight = "200";
+  } else {
+    const readButton = document.createElement("button");
+    readButton.textContent = "Mark as read";
+    readButton.style.marginLeft = "10px";
+    readButton.style.padding = "10px";
+    readButton.addEventListener("click", async () => {
+      await manager.setRead(notification.id);
+      li.remove();
+      newNotification({ ...notification, readAt: Date.now() });
+    });
+    li.appendChild(readButton);
+  }
 
-    ul.appendChild(li);
-  });
+  ul.appendChild(li);
 }
 
-manager.subscribe(() => {
-  renderNotifications();
+const list = await manager.get();
+if (list.length > 0) {
+  notificationBox.style.display = "flex";
+  list.forEach((n) => newNotification(n));
+}
+
+manager.subscribe((notification) => {
+  newNotification(notification);
 });
 
 const button = document.getElementById("send-btn");
@@ -57,9 +57,7 @@ button.addEventListener("click", async () => {
   const title = document.getElementById("title-input").value;
   const message = document.getElementById("msg-input").value;
 
-  if (title === "" || message === "") return;
+  if (!title || !message) return;
 
   await manager.send({ title, message });
 });
-
-renderNotifications();
